01b568be05ee8078f3e0d1866e04678834fcf3e9
Wilson <lwx_324@hotmail.com>
2024-08-13 22:10:49 +0800

    Update set-aws-credentials.sh

--- Server/set-aws-credentials.sh
+++ Server/set-aws-credentials.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export AWS_ACCESS_KEY_ID=AKIA5SY7WSQCMNIBRFSR
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
 export AWS_DEFAULT_REGION=us-east-1
 
 echo "AWS credentials set."
